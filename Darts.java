3fefc6bd8f3fc0003dcd7244efd4d4392eb9054c
conorprunty <conorprunty@users.noreply.github.com>
2017-10-05 10:07:45 +0100

    Initial logic

---
+++ Darts.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package darts;
+
+/**
+ *
+ * @author a990553
+ */
+public class Darts {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Score s = new Score();
+        s.setVisible(true);
+        s.setLocationRelativeTo(null);
+    }
+    
+}
